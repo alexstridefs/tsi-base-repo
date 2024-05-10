275730ea7a693e78cad1a30b654f6361dff1dbcc
Alex Stride <alex.stride@featurespace.co.uk>
2024-05-10 13:54:36 +0100

    example with solution

---
+++ README.md
@@ -0,0 +1,7 @@
+This IDE environment is running in a Linux based virtual machine.
+
+Use `Ctrl + J` to open/close terminal.
+
+You can maximize this pane using the icon at the top right of the IDE.
+
+The menu button located at top left has additional functionalities/customizations.
---
+++ request_sender.ts
@@ -0,0 +1,18 @@
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+/**
+ * Returns status code of request.
+ */
+export async function sendRequest(url: string): Promise<number> {
+    if (Math.random() < 0.01) {
+        // Simulate server error (slow call, 500 response)
+        await delay(1000);
+        return 500;
+    } else {
+        // Simulate server success (fast call, 200 response)
+        await delay(Math.random() * 50);
+        return 200;
+    }
+}
